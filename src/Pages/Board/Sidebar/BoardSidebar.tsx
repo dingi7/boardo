db3cf124bd38c2f5007a363ffd81e96e1cafa19f
mitkoprobul <106108077+MityoDraganov@users.noreply.github.com>
2023-11-20 21:54:37 +0200

    Update BoardSidebar.tsx

--- src/Pages/Board/Sidebar/BoardSidebar.tsx
+++ src/Pages/Board/Sidebar/BoardSidebar.tsx
@@ -1,8 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
 const navLinks = {
-    board: "/board/create",
-    create: "/create"
+    board: "/board",
+    create: "/board/create"
 }
 
 export const BoardSidebar: React.FC<{ boards: { boardName: string, boardId: string }[] }> = ({ boards }): JSX.Element => {
