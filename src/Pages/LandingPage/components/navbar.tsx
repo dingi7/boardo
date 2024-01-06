af2e770ea4e7b576b5374121d59a1ff1c4767c8f
Mityo Draganov <106108077+MityoDraganov@users.noreply.github.com>
2024-01-06 13:29:43 +0200

    LOGOUT

--- src/Pages/LandingPage/components/navbar.tsx
+++ src/Pages/LandingPage/components/navbar.tsx
@@ -1,34 +1,51 @@
 import { Logo } from '../../../Components/ui/logo';
 import { Link } from 'react-router-dom';
 import { Button } from '../../../Components/ui/button';
-import { useIsAuthenticated } from 'react-auth-kit';
+import { useIsAuthenticated, useSignOut } from 'react-auth-kit';
 import React from 'react';
 
 export const Navbar = () => {
     const isAuth = useIsAuthenticated();
-    return (
-        <div className="fixed top-0 w-full h-16 px-4 border-b shadow-sm bg-white flex items-center">
-            <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
-                <Logo />
-                <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-                    {isAuth() ? (
-                        <Button variant={'gray'} size="sm">
-                            <Link to="/dashboard">Dashboard</Link>
-                        </Button>
-                    ) : (
-                        <>
-                            <Button size="sm" variant="outline" asChild>
-                                <Link to="/auth/login">Login</Link>
-                            </Button>
-                            <button className="bg-neutral-200 text-secondary-foreground hover:bg-neutral-300 inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 rounded-md px-3">
-                                <Link to="/auth/register">
-                                    Get Boardo for free
-                                </Link>
-                            </button>
-                        </>
-                    )}
+
+    const signOut = useSignOut()
+
+    const handleSignOut = () => {
+        if (window.confirm("Are you sure you want to log out?")) {
+            signOut();
+        }
+    };
+    
+
+
+        return (
+            <div className="fixed top-0 w-full h-16 px-4 border-b shadow-sm bg-white flex items-center">
+                <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+                    <Logo />
+                    <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+                        {isAuth() ? (
+                            <>
+                                <Button variant={'gray'} size="sm">
+                                    <Link to="/dashboard">Dashboard</Link>
+                                </Button>
+
+                                <Button variant={'gray'} size="sm">
+                                    <a onClick={handleSignOut}>Logout</a>
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                <Button size="sm" variant="outline" asChild>
+                                    <Link to="/auth/login">Login</Link>
+                                </Button>
+                                <button className="bg-neutral-200 text-secondary-foreground hover:bg-neutral-300 inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 rounded-md px-3">
+                                    <Link to="/auth/register">
+                                        Get Boardo for free
+                                    </Link>
+                                </button>
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    };
