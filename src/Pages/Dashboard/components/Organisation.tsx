b11eba73f16768fb2e16e5402ae2f6f4f0c3c97d
Mityo Draganov <106108077+MityoDraganov@users.noreply.github.com>
2024-01-07 22:37:05 +0200

    side bar design

--- src/Pages/Dashboard/components/Organisation.tsx
+++ src/Pages/Dashboard/components/Organisation.tsx
@@ -32,7 +32,7 @@ export const Organisation = ({
             }}
             className='mb-3 pr-[4%]'
         >
-            <h1 className='w-full flex flex-row justify-between font-medium gap-[4%]'>
+            <h1 className='text-xs md:text-base lg:text-text-lg w-full flex flex-row justify-between font-medium gap-[4%]'>
                 <div className='bg-gradient-to-r from-purple-500 to-indigo-600 p-[3%] rounded min-height'>
                     <img
                         src={orgLogo}
@@ -50,16 +50,16 @@ export const Organisation = ({
             {isOpen && (
                 <ul className='mt-[2%] ml-[4%] flex flex-col'>
                     <li className='flex flex-row gap-[5%] p-[4%] hover:bg-teal-100 hover:text-teal-700 rounded'>
-                        <Layout className="w-[20%] aspect-square"/> Boards
+                        <Layout className="w-[30%] md:w-[10%] h-full"/> Boards
+                    </li>
+                    {/* <li className='flex flex-row gap-[5%] p-[4%] hover:bg-teal-100 hover:text-teal-700 rounded'>
+                        <Lightbulb className="w-[40%] md:w-[10%] h-full"/> Brainstorming
+                    </li> */}
+                    <li className='flex flex-row gap-[5%] p-[4%] hover:bg-teal-100 hover:text-teal-700 rounded'>
+                        <Activity className="w-[30%] md:w-[10%] h-full"/> Activity
                     </li>
                     <li className='flex flex-row gap-[5%] p-[4%] hover:bg-teal-100 hover:text-teal-700 rounded'>
-                        <Lightbulb /> Brainstorming
-                    </li>
-                    <li className='flex flex-row gap-[5%] p-[4%] hover:bg-teal-100 hover:text-teal-700 rounded'>
-                        <Activity /> Activity
-                    </li>
-                    <li className='flex flex-row gap-[5%] p-[4%] hover:bg-teal-100 hover:text-teal-700 rounded'>
-                        <Settings /> Settings
+                        <Settings className="w-[30%] md:w-[10%] h-full"/> Settings
                     </li>
                 </ul>
             )}
