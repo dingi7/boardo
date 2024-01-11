053b9210742731124ea951b43aa8dcefddc05743
unknown <bgtrak@gmail.com>
2024-01-11 19:33:03 +0200

    edit styling

--- src/Pages/Dashboard/components/Organization.tsx
+++ src/Pages/Dashboard/components/Organization.tsx
@@ -5,9 +5,9 @@ import {
     ChevronUp,
     ChevronDown,
     Building2,
-} from 'lucide-react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { IOrg } from '../contexts/DashboardContextProvider';
+} from "lucide-react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { IOrg } from "../contexts/DashboardContextProvider";
 
 export const Organization = ({
     orgName,
@@ -35,19 +35,19 @@ export const Organization = ({
                 }
                 onClick();
             }}
-            className='mb-3 pr-[4%]'
+            className="mb-3 pr-[4%]"
         >
-            <h1 className='text-xs md:text-base lg:text-text-lg w-full flex flex-row justify-between font-medium gap-[4%]'>
-                <div className='bg-gradient-to-r from-purple-500 to-indigo-600 p-[3%] rounded min-height'>
-                    <Building2 color='white'></Building2>
+            <h1 className="text-xs md:text-base lg:text-text-lg w-full flex flex-row justify-between font-medium gap-[4%]">
+                <div className="bg-gradient-to-r from-purple-500 to-indigo-600 p-[3%] rounded min-height">
+                    <Building2 color="white"></Building2>
                 </div>
-                <div className='flex flex-row gap-[5%] w-full justify-between'>
-                    {orgName}{' '}
+                <div className="flex flex-row gap-[5%] w-full justify-between">
+                    {orgName}{" "}
                     {expandedOrganizationId === orgId ? (
                         <ChevronUp
                             onClick={(e: any) => {
                                 e.stopPropagation();
-                                setExpandedOrganizationId('');
+                                setExpandedOrganizationId("");
                             }}
                         />
                     ) : (
@@ -62,36 +62,64 @@ export const Organization = ({
             </h1>
 
             {expandedOrganizationId === orgId && (
-                <ul className='mt-[2%] ml-[4%] flex flex-col'>
+                <ul className="mt-[2%] ml-[4%] flex flex-col">
                     <li
-                        className={`flex flex-row gap-[5%] p-[4%] hover:bg-teal-100 hover:text-teal-700 rounded ${
+                        className={`flex flex-row items-center gap-[2%] px-[2%] py-[3%] rounded ${
                             selectedOrganization!._id === orgId &&
-                            pathname === '/dashboard/boards'
-                                ? 'bg-teal-100 text-teal-700'
-                                : ''
+                            pathname === "/dashboard/boards"
+                                ? "bg-blue-100 text-sky-800"
+                                : "hover:bg-sky-50 hover:text-sky-700"
                         }`}
-                        onClick={() => navigate('/dashboard/boards')}
+                        onClick={() => navigate("/dashboard/boards")}
                     >
-                        <Layout className='w-[30%] md:w-[10%] h-full' /> Boards
+                        <Layout
+                            className={`w-[20%] md:w-[10%] ${
+                                selectedOrganization!._id === orgId &&
+                                pathname === "/dashboard/boards"
+                                    ? "bg-blue-100"
+                                    : "hover:bg-sky-50"
+                            }`}
+                        />{" "}
+                        Boards
                     </li>
-                    <li className='flex flex-row gap-[5%] p-[4%] hover:bg-teal-100 hover:text-teal-700 rounded'>
-                        <Activity className='w-[30%] md:w-[10%] h-full' />{' '}
+                    <li
+                        className={`flex flex-row items-center gap-[2%] px-[2%] py-[3%] rounded ${
+                            selectedOrganization!._id === orgId &&
+                            pathname === "/dashboard/activity"
+                                ? "bg-blue-100 text-sky-800"
+                                : "hover:bg-sky-50 hover:text-sky-700"
+                        }`}
+                    >
+                        <Activity
+                            className={`w-[20%] md:w-[10%] ${
+                                selectedOrganization!._id === orgId &&
+                                pathname === "/dashboard/activity"
+                                    ? "bg-blue-100"
+                                    : "hover:bg-sky-50"
+                            }`}
+                        />{" "}
                         Activity
                     </li>
                     <li
-                        className={`flex flex-row gap-[5%] p-[4%] hover:bg-teal-100 hover:text-teal-700 rounded ${
+                        className={`flex flex-row items-center gap-[2%] px-[2%] py-[3%] rounded ${
                             selectedOrganization!._id === orgId &&
-                            pathname === '/dashboard/settings'
-                                ? 'bg-teal-100 text-teal-700'
-                                : ''
+                            pathname === "/dashboard/settings"
+                                ? "bg-blue-100 text-sky-800"
+                                : "hover:bg-sky-50 hover:text-sky-700"
                         }`}
-                        onClick={() =>{
-                            console.log('clicked');
-                            navigate('/dashboard/settings')
-                            
+                        onClick={() => {
+                            console.log("clicked");
+                            navigate("/dashboard/settings");
                         }}
                     >
-                        <Settings className='w-[30%] md:w-[10%] h-full' />{' '}
+                        <Settings
+                            className={`w-[20%] md:w-[10%] ${
+                                selectedOrganization!._id === orgId &&
+                                pathname === "/dashboard/settings"
+                                    ? "bg-blue-100"
+                                    : "hover:bg-sky-50"
+                            }`}
+                        />{" "}
                         Settings
                     </li>
                 </ul>
