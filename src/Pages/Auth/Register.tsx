40d1264146fe19b0894f8bb46a4e4583ab8cff65
Mityo Draganov <106108077+MityoDraganov@users.noreply.github.com>
2024-01-11 21:53:47 +0200

    padding changes

--- src/Pages/Auth/Register.tsx
+++ src/Pages/Auth/Register.tsx
@@ -53,9 +53,9 @@ export const Register = () => {
     };
 
     return (
-        <div className='h-screen bg-white flex justify-center items-center'>
+        <div className='h-screen bg-white flex justify-center items-center pt-[40%] md:pt-[0]'>
             <Navbar></Navbar>
-            <div className='w-[600px] border-1 bg-[#e2e2e2] rounded-md flex flex-col p-12 pb-16 justify-between'>
+            <div className='w-[95%] md:w-[60%] lg:w-[50%] xl:w-[46%] border-1 bg-[#e2e2e2] rounded-md flex flex-col p-12 pb-16 justify-between'>
                 <div className='mx-auto'>
                     <Logo />
                 </div>
--- src/Pages/Board/components/BoardHeader.tsx
+++ src/Pages/Board/components/BoardHeader.tsx
@@ -15,7 +15,7 @@ export const BoardHeader = ({
     setBackgroundUrl,
 }: Props): JSX.Element => {
     return (
-        <div className='bg-black bg-opacity-20 text-white w-full flex items-center justify-between py-[1%] px-16'>
+        <div className='bg-black bg-opacity-20 text-white w-full flex items-center justify-between py-[1%] pt-[10%] sm:pt-[1%] lg:pt-[3%] xl:pt-[2%] 2xl:pt-[1%] px-[5%] '>
             <div className='flex items-center gap-1'>
                 <BoardTitle boardName={boardName} setBoardName={setBoardName} />
             </div>
