3c059bea88772e750283ca1904bc7dda2b19cb21
Kamen Kanev <kamenkanev88@gmail.com>
2024-01-12 18:35:34 +0200

    Refactor BoardSkeleton component

--- src/Pages/Dashboard/pages/BoardSkeleton.tsx
+++ src/Pages/Dashboard/pages/BoardSkeleton.tsx
@@ -1,34 +1,38 @@
-import { Skeleton } from "src/Components/ui/skeleton"
+import { Skeleton } from 'src/Components/ui/skeleton';
 
 export const BoardSkeleton = () => {
+    const array = new Array(8).fill(0);
+
     return (
         <>
             <div className='flex flex-row gap-[2%] items-center'>
                 <Skeleton className='p-[2%] w-[20%] sm:w-[15%] md:w-[10%] lg:w-[8%] xl:w-[6%] aspect-square text-black flex justify-center items-center rounded bg-gradient-to-r from-purple-500 to-indigo-600'>
-                    <Skeleton color='white' className='w-[100%]' />
+                    <Skeleton className='w-full h-full' />
                 </Skeleton>
+
                 <div>
-                <Skeleton className='font-extrabold text-2xl'>
-                </Skeleton></div>
+                    <Skeleton className='font-extrabold text-2xl w-[200px] h-8' />
+                </div>
             </div>
 
             <div className='mt-[4%] w-full'>
-                <Skeleton className='flex flex-row gap-[1%] font-medium text-xl'>
-                    <Skeleton />
-                </Skeleton>
+                <div className='flex flex-row gap-[1%] font-medium text-xl'>
+                    <Skeleton className='w-8 h-8' />
+                    <Skeleton className=' w-full h-8' />
+                </div>
+
                 <Skeleton className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mt-[2%]'>
-
-                    <div
-                        className='group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden'
-                    >
-                        <Skeleton className='absolute inset-0 bg-black/30 group-hover:bg-black/40 transition' />
-                        <Skeleton className='relative font-semibold text-white'>
-
+                    {array.map((index) => (
+                        <Skeleton
+                            key={index}
+                            className='group relative aspect-video bg-no-repeat bg-center bg-cover bg-slate-300 rounded-sm h-[150px] w-full p-2 overflow-hidden'
+                        >
+                            <Skeleton className='absolute inset-0 bg-black/30 group-hover:bg-black/40 transition' />
+                            <Skeleton className='relative font-semibold text-white' />
                         </Skeleton>
-                    </div>
-
+                    ))}
                 </Skeleton>
             </div>
         </>
-    )
-}
+    );
+};
