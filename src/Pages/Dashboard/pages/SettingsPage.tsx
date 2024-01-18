101cfc553f53e8886f01d80cc477f4334ebacca4
Kamen Kanev <kamenkanev88@gmail.com>
2024-01-18 22:48:55 +0200

    Update settings apperance

--- src/Pages/Dashboard/pages/SettingsPage.tsx
+++ src/Pages/Dashboard/pages/SettingsPage.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 // import { useAuthUser } from "react-auth-kit";
-import { useOutletContext } from "react-router-dom";
-import MemberCard from "../components/MemberCard";
-import { Input } from "src/Components/ui/input";
-import { Button } from "src/Components/ui/button";
+import { useOutletContext } from 'react-router-dom';
+import MemberCard from '../components/MemberCard';
+import { Input } from 'src/Components/ui/input';
+import { Button } from 'src/Components/ui/button';
 type Props = {};
 
 export const Settings = (props: Props) => {
@@ -11,7 +11,7 @@ export const Settings = (props: Props) => {
     // const auth = useAuthUser()();
     // const isAuthenticated = auth && auth._id === selectedOrganization.owner;
     const [name, setName] = useState(selectedOrganization.name);
-    const [activeTab, setActiveTab] = useState("members");
+    const [activeTab, setActiveTab] = useState('members');
     const handleTabClick = (tabId: string) => {
         setActiveTab(tabId);
     };
@@ -111,64 +111,64 @@ export const Settings = (props: Props) => {
         //         </div>
         //     )}
         // </div>
-        <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
+        <div className='mb-4 border-b border-gray-200 dark:border-gray-700'>
             <ul
-                className="flex flex-wrap -mb-px text-sm font-medium text-center"
-                role="tablist"
+                className='flex flex-wrap -mb-px text-sm font-medium text-center'
+                role='tablist'
             >
-                <li className="me-2" role="presentation">
+                <li className='me-2' role='presentation'>
                     <button
                         className={`inline-block p-4 border-b-2 rounded-t-lg ${
-                            activeTab === "members"
-                                ? "border-blue-500 text-blue-500 font-semibold"
-                                : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                            activeTab === 'members'
+                                ? 'border-blue-500 text-blue-500 font-semibold'
+                                : 'hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
                         }`}
-                        id="members-tab"
-                        data-tabs-target="#members"
-                        type="button"
-                        role="tab"
-                        aria-controls="members"
-                        aria-selected={activeTab === "members"}
-                        onClick={() => handleTabClick("members")}
+                        id='members-tab'
+                        data-tabs-target='#members'
+                        type='button'
+                        role='tab'
+                        aria-controls='members'
+                        aria-selected={activeTab === 'members'}
+                        onClick={() => handleTabClick('members')}
                     >
-                        <span className="text-lg">Members</span>
+                        <span className='text-lg'>Members</span>
                     </button>
                 </li>
-                <li className="me-2" role="presentation">
+                <li className='me-2' role='presentation'>
                     <button
                         className={`inline-block p-4 border-b-2 rounded-t-lg ${
-                            activeTab === "settings"
-                                ? "border-blue-500 text-blue-500 font-semibold"
-                                : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                            activeTab === 'settings'
+                                ? 'border-blue-500 text-blue-500 font-semibold'
+                                : 'hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
                         }`}
-                        id="settings-tab"
-                        data-tabs-target="#settings"
-                        type="button"
-                        role="tab"
-                        aria-controls="settings"
-                        aria-selected={activeTab === "settings"}
-                        onClick={() => handleTabClick("settings")}
+                        id='settings-tab'
+                        data-tabs-target='#settings'
+                        type='button'
+                        role='tab'
+                        aria-controls='settings'
+                        aria-selected={activeTab === 'settings'}
+                        onClick={() => handleTabClick('settings')}
                     >
-                        <span className="text-lg">Settings</span>
+                        <span className='text-lg'>Settings</span>
                     </button>
                 </li>
             </ul>
-            <div id="default-tab-content">
+            <div id='default-tab-content'>
                 <div
                     className={`h-full overflow-y-auto p-6 ${
-                        activeTab === "members" ? "block" : "hidden"
+                        activeTab === 'members' ? 'block' : 'hidden'
                     }`}
-                    id="members"
-                    role="tabpanel"
-                    aria-labelledby="members-tab"
+                    id='members'
+                    role='tabpanel'
+                    aria-labelledby='members-tab'
                 >
-                    <section className="flex flex-col gap-[1rem] mt-[4%]">
-                        <h2 className="font-bold text-[24px]">
+                    <section className='flex flex-col gap-[1rem] mt-[4%]'>
+                        <h2 className='font-bold text-[24px]'>
                             Organisation members
                         </h2>
 
-                        <div className="flex flex-col gap-[0.2rem]">
-                            <ul className="list-none">
+                        <div className='flex flex-col gap-[0.2rem]'>
+                            <ul className='list-none'>
                                 {selectedOrganization.members.filter(
                                     (member: any) =>
                                         member._id ===
@@ -186,7 +186,7 @@ export const Settings = (props: Props) => {
                                             ></MemberCard>
                                         ))
                                 ) : (
-                                    <li className="font-bold">
+                                    <li className='font-bold'>
                                         No members yet!
                                     </li>
                                 )}
@@ -196,23 +196,23 @@ export const Settings = (props: Props) => {
                 </div>
                 <div
                     className={`h-full overflow-y-auto p-6 ${
-                        activeTab === "settings" ? "block" : "hidden"
+                        activeTab === 'settings' ? 'block' : 'hidden'
                     }`}
-                    id="settings"
-                    role="tabpanel"
-                    aria-labelledby="settings-tab"
+                    id='settings'
+                    role='tabpanel'
+                    aria-labelledby='settings-tab'
                 >
-                    <section className="flex flex-col mt-[4%]">
-                        <h2 className="font-bold text-[24px]">
+                    <section className='flex flex-col mt-[4%]'>
+                        <h2 className='font-bold text-[24px]'>
                             Organisation settings
                         </h2>
 
-                        <div className="flex flex-col gap-[4%]">
-                            <ul className="list-none mt-[4%]">
-                                <li className="flex flex-row justify-between items-center gap-2">
-                                    <label htmlFor="nameInput">Name: </label>
+                        <div className='flex flex-col gap-[4%]'>
+                            <ul className='list-none mt-[4%]'>
+                                <li className='flex flex-row justify-between items-center gap-2'>
+                                    <label htmlFor='nameInput'>Name: </label>
                                     <Input
-                                        id="nameInput"
+                                        id='nameInput'
                                         value={name}
                                         onChange={(e) =>
                                             setName(e.target.value)
@@ -220,9 +220,12 @@ export const Settings = (props: Props) => {
                                     ></Input>
                                 </li>
                             </ul>
-                            <div className="flex flex-row gap-[1rem] mt-[4%]">
-                                <Button variant="outline">Reset</Button>
-                                <Button variant="primary">Save</Button>
+                            <div className='flex flex-row gap-[1rem] mt-[4%]'>
+                                {/* <Button variant='outline'>Reset</Button> */}
+                                <Button variant={'destructive'}>
+                                    Delete organization
+                                </Button>
+                                <Button variant='primary'>Save Changes</Button>
                             </div>
                         </div>
                     </section>
