ff28d8441f24e75a315f06b34a038229004e0e38
Kamen Kanev <kamenkanev88@gmail.com>
2024-01-22 20:35:27 +0200

    Update Card component styling

--- src/Pages/Board/components/Card/Card.tsx
+++ src/Pages/Board/components/Card/Card.tsx
@@ -48,7 +48,7 @@ export const Card: React.FC<CardItem> = ({
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                     ref={provided.innerRef}
-                    className='flex w-full h-9 items-center gap-[10px] px-[10px] py-[2%] relative bg-slate-100 rounded-[7px] overflow-hidden border-black cursor-pointer '
+                    className='flex w-full h-auto items-center gap-[10px] px-[10px] py-[2%] relative bg-slate-100 rounded-[7px] overflow-hidden border-black cursor-pointer '
                     onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}
                 >
