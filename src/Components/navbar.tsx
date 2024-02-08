10ab43cf52c174fc13a90cd2525cc30d649dbd46
Kamen Kanev <kamenkanev88@gmail.com>
2024-02-08 22:25:51 +0200

    Update navbar.tsx

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -1,10 +1,10 @@
-import { Logo } from "./ui/logo";
-import { Link, useNavigate } from "react-router-dom";
-import { Button } from "./ui/button";
-import { useAuthUser, useIsAuthenticated, useSignOut } from "react-auth-kit";
-import React from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { User2Icon } from "lucide-react";
+import { Logo } from './ui/logo';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button } from './ui/button';
+import { useAuthUser, useIsAuthenticated, useSignOut } from 'react-auth-kit';
+import React from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { User2Icon } from 'lucide-react';
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -12,8 +12,8 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "./dropdown";
-import { useAuth } from "src/Pages/Auth/hooks/useAuth";
+} from './dropdown';
+import { useAuth } from 'src/Pages/Auth/hooks/useAuth';
 
 export const Navbar = () => {
     const isAuth = useIsAuthenticated();
@@ -22,21 +22,21 @@ export const Navbar = () => {
     const signOut = useSignOut();
 
     const handleSignOut = () => {
-        if (window.confirm("Are you sure you want to log out?")) {
+        if (window.confirm('Are you sure you want to log out?')) {
             signOut();
-            navigate("/");
+            navigate('/');
         }
     };
 
     return (
-        <div className="w-full px-4 py-2 border-b shadow-sm bg-white flex items-center">
-            <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <div className='w-full px-4 py-2 border-b shadow-sm bg-white flex items-center'>
+            <div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
                 <Logo />
-                <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+                <div className='space-x-4 md:block md:w-auto flex items-center justify-between w-full'>
                     {isAuth() ? (
-                        <div className="flex items-center gap-2">
-                            <Button variant={"gray"} size="sm" asChild>
-                                <Link to="/dashboard">Dashboard</Link>
+                        <div className='flex items-center gap-2'>
+                            <Button variant={'gray'} size='sm' asChild>
+                                <Link to='/dashboard'>Dashboard</Link>
                             </Button>
 
                             {/* <Button
@@ -54,7 +54,7 @@ export const Navbar = () => {
                                     <Avatar>
                                         <AvatarImage
                                             src={`https://api.dicebear.com/7.x/initials/svg?seed=${authUser?.username}&radius=50&backgroundColor=a3a3a3&fontSize=35&bold=true`}
-                                            alt="User avatar"
+                                            alt='User avatar'
                                         />
                                     </Avatar>
                                 </DropdownMenuTrigger>
@@ -63,9 +63,11 @@ export const Navbar = () => {
                                         My Account
                                     </DropdownMenuLabel>
                                     <DropdownMenuSeparator />
-                                    <DropdownMenuItem>
-                                        <Link to="/profile">Profile</Link>
-                                    </DropdownMenuItem>
+                                    <Link to='/profile'>
+                                        <DropdownMenuItem>
+                                            Profile
+                                        </DropdownMenuItem>
+                                    </Link>
                                     <DropdownMenuItem onClick={handleSignOut}>
                                         Logout
                                     </DropdownMenuItem>
@@ -74,11 +76,11 @@ export const Navbar = () => {
                         </div>
                     ) : (
                         <>
-                            <Button size="sm" variant="outline" asChild>
-                                <Link to="/auth/login">Login</Link>
+                            <Button size='sm' variant='outline' asChild>
+                                <Link to='/auth/login'>Login</Link>
                             </Button>
-                            <Button size="sm" variant="primary" asChild>
-                                <Link to="/auth/register">
+                            <Button size='sm' variant='primary' asChild>
+                                <Link to='/auth/register'>
                                     Get Boardo for free
                                 </Link>
                             </Button>
