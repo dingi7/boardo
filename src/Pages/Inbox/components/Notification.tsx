0ccabe6ef337126b89feb8424a8a11466dba8b8f
Mityo Draganov <mityodraganow@gmail.com>
2024-04-25 16:58:46 +0300

    fixed width for inbox

--- src/Pages/Inbox/components/Notification.tsx
+++ src/Pages/Inbox/components/Notification.tsx
@@ -6,23 +6,29 @@ import {
 } from '@radix-ui/react-tooltip';
 import { X } from 'lucide-react';
 import { DropdownMenuSeparator } from 'src/Components/dropdown';
-import { Card, CardContent, CardHeader } from 'src/Components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from 'src/Components/ui/card';
 import { INotification } from 'src/Interfaces/INotification';
 
 export const Notification = ({
-  _id,
-  isRead,
-  title,
-  description,
-  markCurrentNotificationAsRead,
-  createdAt
-}: INotification) => {
+    _id,
+    isRead,
+    title,
+    description,
+    markCurrentNotificationAsRead,
+    createdAt,
+  }: INotification) => {
     const createdDate = new Date(createdAt);
-  return (
-    <div className='w-full'>
-      <DropdownMenuSeparator />
+  
+    return (
+        <div className='w-full'>
+            <DropdownMenuSeparator />
       <Card
-        className={`relative border-x-0 border-y-4 p-2 ${
+        className={`relative border-x-0 border-y-4 p-2  w-full max-w-full ${
           isRead ? 'opacity-60' : ''
         }`}
       >
@@ -35,25 +41,24 @@ export const Notification = ({
               >
                 <X />
               </TooltipTrigger>
-
+  
               <TooltipContent>
                 <p className='p-2 rounded-md bg-slate-100'>Mark as read</p>
               </TooltipContent>
             </Tooltip>
           </TooltipProvider>
         )}
-        <CardHeader className='flex flex-row items-center gap-2 px-2 py-0'>
-          <div className='flex gap-2'>
-              <span className='font-semibold text-md'>{title}</span>
-              <span className='opacity-80'>{createdDate.toLocaleDateString()}</span>
-          </div>
-        </CardHeader>
-        <CardContent className='px-2 py-0 w-36'>
-          <div className='text-sm max-w-40 w-36'>
-            <p className='break-words  max-w-[260px]'>{description}</p>
-          </div>
-        </CardContent>
+  
+        <div className='flex gap-2'>
+          <span className='font-semibold text-md'>{title}</span>
+          <span className='opacity-80'>{createdDate.toLocaleDateString()}</span>
+        </div>
+  
+        <div className='w-full h-16 max-w-full overflow-hidden' style={{width: '100%'}}>
+          <p style={{width: '100%'}}>{description}</p>
+        </div>
       </Card>
-    </div>
-  );
-};
+      </div>
+    );
+  };
+  
